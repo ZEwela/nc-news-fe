50eed38954d54e011c2c34e42607dcfa000a89f3
Ewelina <ewelinazawol@gmail.com>
2024-03-12 12:53:02 +0000

    Fix: voting element (user can vote once, do not allow negative total votes

--- src/components/VoteElement.jsx
+++ src/components/VoteElement.jsx
@@ -3,7 +3,19 @@ import { useState } from "react"
 const VoteElement = ({votes, handleVoting}) => {
 
     const [votesDisplay, setVotesDisplay] = useState(votes)   
+    const [upvoteDisabled, setUpvoteDisabled] = useState(false)
+    const [downvoteDisabled, setDownvoteDisabled] = useState(votes <= 0)
+
+    console.log(!votes)
+
     const handleVotesDisplayChange = (newVote) => {
+        if (newVote === 1) {
+            setUpvoteDisabled((currState) => !currState)
+            setDownvoteDisabled((currState) => currState ? !currState : currState)
+        } else {
+            setDownvoteDisabled((currState) => !currState)
+            setUpvoteDisabled((currState) => currState ? !currState : currState)
+        }
         setVotesDisplay(currVotesDisplay => {
             return currVotesDisplay + newVote
         })
@@ -14,9 +26,9 @@ const VoteElement = ({votes, handleVoting}) => {
     return (
         <div className="border">
             <section className="vote-element">
-                 <button onClick={() => {handleVoting(1), handleVotesDisplayChange(1)}}>+</button>
+                 {<button disabled={upvoteDisabled} onClick={() => {handleVoting(1), handleVotesDisplayChange(1)}}>+</button>}
                  {votesDisplay !== 0 && <p>Sentiment: {votesDisplay}</p>}
-                 <button onClick={() =>{ handleVoting(-1), handleVotesDisplayChange(-1)}}>-</button>
+                 <button disabled={downvoteDisabled} onClick={() =>{ handleVoting(-1), handleVotesDisplayChange(-1)}}>-</button>
             </section>
         </div>
     )
